0dc4c096762e8c0f748cec4cce170e14c86dc70c
Yousef Khani <cfyakan@gmail.com>
2026-06-11 20:26:27 +0330

    Update Player.cs

--- GuessGame/Player.cs
+++ GuessGame/Player.cs
@@ -1,1 +1,13 @@
+class Player
+{
+    public string Name { get; set; }
+    public int Attempts { get; set; }
+    public int BestScore { get; set; }
 
+    public Player(string name)
+    {
+        Name = name;
+        Attempts = 0;
+        BestScore = int.MaxValue;
+    }
+}
