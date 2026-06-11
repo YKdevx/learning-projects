6639238d570f74b925c7212cce19cb9432d24485
Yousef Khani <cfyakan@gmail.com>
2026-06-11 20:22:03 +0330

    Update Program.cs

--- GuessGame/Program.cs
+++ GuessGame/Program.cs
@@ -1,1 +1,15 @@
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Guess Game!");
 
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Player player = new Player(name);
+
+        Game game = new Game(player);
+        game.Start();
+    }
+}
